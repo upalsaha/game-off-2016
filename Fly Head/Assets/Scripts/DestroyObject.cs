a7b0cf204f217a89b0ea1619810b9f2a92f1d8da
Tyler Robinson <tlr8cn@virginia.edu>
2016-11-10 15:21:00 -0500

    head falls from pipe at beginning of level

---
+++ Fly Head/Assets/Scripts/DestroyObject.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyObject : MonoBehaviour {
+
+	int timer;
+	public int timer_cap;
+
+	// Use this for initialization
+	void Start () {
+		
+		timer = 0;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(timer > timer_cap)
+			Destroy (transform.gameObject);
+
+
+		timer++;
+	
+	}
+}
