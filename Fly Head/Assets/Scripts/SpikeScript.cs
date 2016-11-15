5de9e9e70dc0cfb749f08d0a9ad3e4217ae144fb
Tyler Robinson <tlr8cn@virginia.edu>
2016-11-14 22:31:10 -0500

    +New level featuring fans & untextured spikes /n +gave entry/exit pipe a rusty color

---
+++ Fly Head/Assets/Scripts/SpikeScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SpikeScript : MonoBehaviour {
+
+	public int killTimerCap;
+	
+	bool playerDead;
+	int killTimer;
+
+	// Use this for initialization
+	void Start () {
+		killTimer = 0;
+		playerDead = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(playerDead) {
+			if(killTimer < killTimerCap) {
+				killTimer++;
+			} else {
+				SceneManager.LoadScene("Fan_Spike_Level1");
+			}
+		}
+	}
+
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if(col.gameObject.tag == "Player") {
+   			col.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+   			playerDead = true;
+   		}
+	}
+
+}
