d276fb3841531933db3226792419650323ec8f79
Tyler Robinson <tlr8cn@virginia.edu>
2016-11-23 14:52:37 -0500

    exit pipes have been added

---
+++ Fly Head/Assets/Scripts/ExitPipe.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ExitPipe : MonoBehaviour {
+
+	public Object nextLevel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if(col.gameObject.tag == "Player") {
+			SceneManager.LoadScene(nextLevel.name);
+		}
+	}
+}
---
+++ Fly Head/Assets/Scripts/ExitPressurePlate.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitPressurePlate : MonoBehaviour {
+
+	public Transform pipeCap;
+	Transform rotatePoint;
+
+	Vector3 initialCapPosition;
+
+	bool bodyOnPlate;
+
+	bool capOpen;
+
+	// Use this for initialization
+	void Start () {
+		rotatePoint = pipeCap.GetChild(0);
+
+		bodyOnPlate = false;
+
+		capOpen = false;
+
+		initialCapPosition = pipeCap.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(bodyOnPlate) {
+			if(pipeCap.rotation.eulerAngles.z == 0 || pipeCap.rotation.eulerAngles.z >= 270f)
+				pipeCap.RotateAround(new Vector3(rotatePoint.position.x, rotatePoint.position.y, rotatePoint.position.z), Vector3.forward, -1);
+		} else if(!bodyOnPlate && capOpen) {
+			if(pipeCap.rotation.eulerAngles.z <= 360f && pipeCap.rotation.eulerAngles.z >= 355f) {
+		    	capOpen = false;
+		    	pipeCap.eulerAngles = new Vector3(0f, 0f, 0f);
+		    	pipeCap.position = initialCapPosition;
+			} else {
+				pipeCap.RotateAround(new Vector3(rotatePoint.position.x, rotatePoint.position.y, rotatePoint.position.z), Vector3.forward, 4);
+			}
+		}
+
+	}
+
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if(col.gameObject.name == "Body" || col.gameObject.name == "Body(Clone)") {
+			bodyOnPlate = true;
+			capOpen = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D col) {
+		if(col.gameObject.name == "Body" || col.gameObject.name == "Body(Clone)") {
+			bodyOnPlate = false;
+		}
+	}
+}
