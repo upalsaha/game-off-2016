08bfe6f4df38198cb52c74136af81edce44979e8
Tyler Robinson <tlr8cn@virginia.edu>
2016-11-26 00:00:50 -0500

    MOAR BLOOD & other things I think. I can't really remember

--- Fly Head/Assets/Scripts/SpikeScript.cs
+++ Fly Head/Assets/Scripts/SpikeScript.cs
@@ -50,6 +50,7 @@ public class SpikeScript : MonoBehaviour {
 
    			if(col.gameObject.name == "Body" || col.gameObject.name == "Body(Clone)") {
    				col.gameObject.GetComponent<BodyController>().bodyBeingControlled = false;
+   				col.gameObject.GetComponent<BodyController>().deadAF = true;
    			}
    		}
 	}
